94281b045010e8fc4fb26bf4f898dbaac1f3f3db
Chase Roberts <keeper6928@gmail.com>
2018-02-01 19:13:37 -0500

    pep8 on setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 
 setup(
-  name='mltest',
-  version='0.0.5',
-  packages=['mltest'],
-  install_requires=[
-    "tensorflow",
-    "numpy"
-  ],
-  license="MIT",
-  author="Chase Roberts",
-  author_email="keeper6928@gmail.com"
+    name='mltest',
+    version='0.0.6',
+    packages=['mltest'],
+    license="MIT",
+    author="Chase Roberts",
+    author_email="keeper6928@gmail.com"
 )
